43e6939f17333dca396847feee9aa4ac657a739f
nagamoto <koutaro.n.1056@gmail.com>
2023-01-25 23:33:08 +0900

    chapter8の練習問題の修正と全体の修復

--- src/Cleric.java
+++ src/Cleric.java
@@ -1,17 +1,25 @@
+import java.util.Random;
+
 public class Cleric {
     String name;
     int hp;
-    final int MAXHP = 50;
+    final int MAX_HP = 50;
     int mp;
-    final int MAXMP = 10;
+    final int MAX_MP = 10;
 
     public void selfAid() {
+        System.out.println(this.name + "はセルフエイドを唱えた！");
         this.mp -= 5;
-        this.hp += 50;
+        this.hp = this.MAX_HP;
+        System.out.println("HPが最大まで回復した");
     }
 
     public int pray(int sec) {
-        int correctionPoint = sec + new java.util.Random().nextInt(3);
-        return correctionPoint;
+        System.out.println(this.name + "は" + sec + "秒間天に祈った！");
+        int correctionPoint = sec + new Random().nextInt(3);
+        int recoverActual = Math.min(this.MAX_MP - this.mp, correctionPoint);
+        this.mp += recoverActual;
+        System.out.println("MPが" + recoverActual + "回復した");
+        return recoverActual;
     }
 }
