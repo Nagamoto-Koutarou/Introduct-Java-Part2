0c1228f34b428d0fa0f41c3bf4efcf4369d32cb5
nagamoto <koutaro.n.1056@gmail.com>
2023-01-27 22:11:37 +0900

    chapter10の練習問題

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,9 @@ public class Main {
         h.name = "ミナト";
         h.hp = 100;
 
-        Matango m1 = new Matango();
-        m1.hp = 50;
-        m1.suffix = 'A';
+        Matango m1 = new Matango('A');
 
-        Matango m2 = new Matango();
-        m2.hp = 40;
-        m2.suffix = 'B';
-
+        Matango m2 = new Matango('B');
         h.slip();
         m1.run();
         m2.run();
